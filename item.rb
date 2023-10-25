6f481972cf59490c9fa3e634572f584f730c1e39
Mohammad Kazim <kazim.mohammadi110@gmail.com>
2023-10-25 13:21:47 +0430

    Update item.rb

--- item.rb
+++ item.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'date'
 
 # This is the Item class, which represents an item with attributes like author, label, genre, and published date.
@@ -48,4 +46,3 @@ class Item
 
   private :can_be_archived?
 end
-
