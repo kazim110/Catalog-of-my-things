b60546a5e15bef73e39f3360367e416bb3439440
Mohammad Kazim <kazim.mohammadi110@gmail.com>
2023-10-25 13:19:35 +0430

    Update item.rb

--- item.rb
+++ item.rb
@@ -1,25 +1,51 @@
+# frozen_string_literal: true
+
 require 'date'
 
 # This is the Item class, which represents an item with attributes like author, label, genre, and published date.
 class Item
-  attr_accessor :author, :label, :genre, :published_date
-  attr_reader :archived
+  attr_accessor(
+    :genre,
+    :author,
+    :source,
+    :label,
+    :publish_date
+  )
 
-  def initialize(published_date, archived: false)
-    @id = rand(1...100)
-    @genre = nil
-    @author = nil
-    @source = nil
-    @label = nil
-    @published_date = Date.parse(published_date)
-    @archived = archived
+  def initialize(genre, author, source, label, publish_date)
+    @id = Math.rand(1..100)
+    @archived = false
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
   end
 
   def can_be_archived?
-    (Date.today.year - @published_date.year).to_i > 10
+    Date.new.year - @publish_date.year > 10
   end
 
   def move_to_archive
     @archived = true if can_be_archived?
   end
+
+  def add_genre
+    @genre.add_item(self)
+  end
+
+  def add_author
+    @author.add_item(self)
+  end
+
+  def add_label
+    @label.add_item(self)
+  end
+
+  def add_source
+    @source.add_item(self)
+  end
+
+  private :can_be_archived?
 end
+
