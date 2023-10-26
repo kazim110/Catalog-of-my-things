ee14ba203cc6a5fbfd7b7630572fabbddea63030
unknown <macherki.nessrine@yahoo.com>
2023-10-26 21:15:24 +0100

    fix linters

--- game.rb
+++ game.rb
@@ -1,12 +1,8 @@
 # Game class represents a specific type of item with additional properties.
-
-require_relative 'item'
-require 'date'
-
 class Game < Item
   attr_accessor :last_played_at, :multiplayer
 
-  def initialize(title, genre, author, source, label, publish_date, last_played_at, multiplayer)
+  def initialize(title, publish_date, last_played_at,multiplayer)
     super(genre, author, source, label, publish_date)
     @title = title
     @last_played_at = last_played_at
