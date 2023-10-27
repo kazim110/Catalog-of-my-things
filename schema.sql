3deb13ce05b7d441940510373fd9ae1105d35a36
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-10-27 11:01:12 -0600

    Add genres & music album tables to schema

--- schema.sql
+++ schema.sql
@@ -6,6 +6,11 @@ CREATE TABLE labels (
   color VARCHAR(30),
 );
 
+CREATE TABLE genres (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE Books(
   id SERIAL PRIMARY KEY,
   genre_id INT,
@@ -18,4 +23,26 @@ CREATE TABLE Books(
   FOREIGN KEY(genre_id) REFERENCES genres(id),
   FOREIGN KEY(author_id) REFERENCES authors(id),
   FOREIGN KEY(label_id) REFERENCES labels(id)
+);
+
+CREATE TABLE music_albums (
+  id BIGSERIAL PRIMARY KEY,
+  genre_id INT NOT NULL,
+  author_id INT NOT NULL,
+  source_id INT NOT NULL,
+  label_id INT NOT NULL,
+  published_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  CONSTRAINT fk_genre_id
+    FOREIGN KEY (genre_id)
+    REFERENCES genres(id)
+  CONSTRAINT fk_author_id
+    FOREIGN KEY (author_id)
+    REFERENCES authors(id)
+  CONSTRAINT fk_source_id
+    FOREIGN KEY (source_id)
+    REFERENCES sources(id)
+  CONSTRAINT fk_label_id
+    FOREIGN KEY label_id
+    REFERENCES labels(id)
 );
