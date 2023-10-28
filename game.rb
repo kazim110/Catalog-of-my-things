6b539791c0fa41433ee5583fc69e3edad1615624
unknown <macherki.nessrine@yahoo.com>
2023-10-28 10:31:19 +0100

    display list of games

--- game.rb
+++ game.rb
@@ -1,16 +1,22 @@
 require_relative 'item'
-class Game < Item
-  attr_accessor :last_played_at, :multiplayer
 
-  def initialize(title, publish_date, last_played_at, multiplayer)
-    super(genre, author, source, label, publish_date)
-    @title = title
+class Game < Item
+  attr_accessor :last_played_at, :multiplayer, :title, :author
+
+  def initialize(title, genre, last_played_at, multiplayer, author, source, label, publish_date)
+    super(title, genre, author, source, label)
     @last_played_at = last_played_at
     @multiplayer = multiplayer
   end
 
   def can_be_archived?
     super && (Date.new.year - @last_played_at.year > 2)
+  end
+
+  # You can define a class method to add a game to a collection of games.
+  def self.add_game(games, title, genre, last_played_at, multiplayer, author, source, label, publish_date)
+    new_game = Game.new(title, genre, last_played_at, multiplayer, author, source, label, publish_date)
+    games << new_game
   end
 end
 
@@ -27,5 +33,32 @@ def add_game
   print 'Is the game multiplayer? (true/false): '
   multiplayer = gets.chomp.downcase == 'true'
 
-  Game.add_game(@games, title, genre, last_played_at, multiplayer)
+  print 'Enter the author: '
+  author = gets.chomp
+
+  print 'Enter the source: '
+  source = gets.chomp
+
+  print 'Enter the label: '
+  label = gets.chomp
+
+  print 'Enter the publish date (YYYY-MM-DD): '
+  publish_date = Date.parse(gets.chomp)
+
+  Game.add_game(@games, title, genre, last_played_at, multiplayer, author, source, label, publish_date)
 end
+
+def list_games
+    if @games.empty?
+      puts "No games available."
+    else
+      puts "List of Games:"
+      @games.each_with_index do |game, index|
+        puts "#{index + 1}. Title: #{game.title}"
+        puts "   Genre: #{game.genre}"
+        puts "   Last Played Date: #{game.last_played_at}"
+        puts "   Multiplayer: #{game.multiplayer ? 'Yes' : 'No'}"10
+        puts '------------------------------------'
+      end
+    end
+  end
